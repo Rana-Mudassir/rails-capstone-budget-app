7a5fb781fdf5618dabbbd75f48b05d0a16da5241
Rana-Mudassir <mudassirnazird22@gmail.com>
2023-08-05 03:42:08 +0500

    Added tests

--- Gemfile
+++ Gemfile
@@ -51,6 +51,9 @@ gem 'bootsnap', require: false
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
+  gem 'factory_bot_rails'
+  gem 'faker'
+  gem 'rspec-rails', '~> 5.0'
 end
 
 group :development do
@@ -67,10 +70,19 @@ end
 group :test do
   # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
   gem 'capybara'
+  gem 'rails-controller-testing'
   gem 'selenium-webdriver'
+  gem 'shoulda-matchers'
   gem 'webdrivers'
 end
 
 # Gems i Added
 
 gem 'devise'
+
+group :development, :test do
+  gem 'capybara'
+  gem 'factory_bot_rails'
+  gem 'faker'
+  gem 'rspec-rails', '~> 5.0'
+end
---
+++ spec/controllers/entities_controller_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+
+RSpec.describe EntitiesController, type: :controller do
+  let(:user) { create(:user) }
+  let(:group) { create(:group, user:) }
+
+  before do
+    sign_in user
+  end
+
+  describe 'GET #index' do
+    it 'renders the index template' do
+      get :index, params: { group_id: group.id }
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'GET #new' do
+    it 'assigns a new entity to @entity' do
+      get :new, params: { group_id: group.id }
+      expect(assigns(:entity)).to be_a_new(Entity)
+    end
+
+    it 'renders the new template' do
+      get :new, params: { group_id: group.id }
+      expect(response).to render_template(:new)
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid params' do
+      it 'creates a new entity' do
+        expect do
+          post :create, params: { group_id: group.id, entity: attributes_for(:entity, user:) }
+        end.to change(Entity, :count).by(1)
+      end
+
+      it 'associates the new entity with the group' do
+        post :create, params: { group_id: group.id, entity: attributes_for(:entity, user:) }
+        expect(assigns(:entity).groups).to include(group)
+      end
+
+      it 'redirects to the index page of group entities' do
+        post :create, params: { group_id: group.id, entity: attributes_for(:entity, user:) }
+        expect(response).to redirect_to(group_entities_path(group_id: group.id))
+      end
+    end
+
+    context 'with invalid params' do
+      it 'does not create a new entity' do
+        expect do
+          post :create, params: { group_id: group.id, entity: attributes_for(:entity, user:, name: nil) }
+        end.not_to change(Entity, :count)
+      end
+
+      it 're-renders the new template' do
+        post :create, params: { group_id: group.id, entity: attributes_for(:entity, user:, name: nil) }
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+end
